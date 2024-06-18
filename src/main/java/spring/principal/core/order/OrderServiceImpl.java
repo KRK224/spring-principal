0629b91d5eff04f0ca9543d085689c6111f86c56
krk224 <hihas93@naver.com>
2024-06-18 11:17:57 +0900

    [feat] 의존관계 자동 주입 - 생성자, 수정자 주입

--- src/main/java/spring/principal/core/order/OrderServiceImpl.java
+++ src/main/java/spring/principal/core/order/OrderServiceImpl.java
@@ -10,18 +10,42 @@ import spring.principal.core.order.interfaces.OrderService;
 
 @Component
 public class OrderServiceImpl implements OrderService {
-    
-    private final MemberRepository memberRepository;
+
     /**
      * MemberRepository가 추상 인터페이스 뿐만 아니라 구체 클래스에도 의존한다.
      * => 구체 클래스를 지워서 DIP를 지킨다? => null point exception 발생.
      */
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
 //    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private final DiscountPolicy discountPolicy;
+    
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
+
+    /**
+     * 수정자 주입 테스트: 수정자 주입 시에 Component 등록과 의존 관계 주입이 2단계로 이루어진다.
+     */
+    @Autowired
+    public void setMemberRepository(MemberRepository memberRepository) {
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
 
     @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        System.out.println("discountPolicy = " + discountPolicy);
+        this.discountPolicy = discountPolicy;
+    }
+
+    /**
+     * 생성자 주입 시에는 Component 빈 등록과 의존 관계 주입이 동시에 이루어진다.
+     * new OrderServiceImpl(ac.getBean(MemberRepository.class), ac.getBean(DiscountPolicy.class));
+     * @param memberRepository
+     * @param discountPolicy
+     */
+    // 생성자가 하나인 경우, Autowired를 생략 가능하다.
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+
+        System.out.println("1. OrderServiceImpl.OrderServiceImpl");
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
