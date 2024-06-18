93aedb04fb1970dfd35142fe20447e551aeac78f
krk224 <hihas93@naver.com>
2024-06-18 11:36:40 +0900

    [feat] 의존관계 자동 주입 - 필드 주입 DI 프레임워크가 없으면 아무것도 할 수 없다!

--- src/test/java/spring/principal/core/scan/AutoAppConfigTest.java
+++ src/test/java/spring/principal/core/scan/AutoAppConfigTest.java
@@ -5,7 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.principal.core.AutoAppConfig;
+import spring.principal.core.member.interfaces.MemberRepository;
 import spring.principal.core.member.interfaces.MemberService;
+import spring.principal.core.order.interfaces.OrderService;
 
 public class AutoAppConfigTest {
     @Test
@@ -13,5 +15,9 @@ public class AutoAppConfigTest {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderService orderService = ac.getBean(OrderService.class);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberService.getMemberRepository());
+
     }
 }
