9b83e42ab084a2e49a175826e79ba3bd53c438ed
krk224 <hihas93@naver.com>
2024-06-18 12:17:22 +0900

    [feat] 의존 관계 자동 주입 - 옵션 처리

---
+++ src/test/java/spring/principal/core/autowired/AutoWiredTest.java
@@ -0,0 +1,36 @@
+package spring.principal.core.autowired;
+
+import jakarta.annotation.Nullable;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.principal.core.member.Member;
+
+public class AutoWiredTest {
+
+    @Test
+    void AutoWiredOption() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+        @Autowired(required = false)
+        public void setNoBean1(Member member) {
+            // Member는 Spring Container가 관리하지 않는 객체이다.
+            System.out.println("member1 = " + member);
+        }
+
+        @Autowired
+        public void setNoBean2(@Nullable Member member2) {
+            System.out.println("member2 = " + member2);
+        }
+
+        @Autowired
+        public void setNoBean3(Optional<Member> member3) {
+            System.out.println("member3 = " + member3);
+        }
+
+
+    }
+}
