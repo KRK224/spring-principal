f09c96c0ab5c81a77a1914062eef275dafb93ee0
krk224 <hihas93@naver.com>
2024-06-22 12:15:55 +0900

    [feat] 프로토타입 스코프 - 싱글톤과 함께 사용할 경우의 문제점 프로토타입은 스프링 컨테이너에서 호출 시점에 새로 생성해서 반환한다. 하지만, 싱글톤 빈의 의존관계로 설정이 되면 싱글톤은 한번만 생성되기 때문에 프로토타입이 여러번 호출될 일이 없다.

---
+++ src/test/java/spring/principal/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,95 @@
+package spring.principal.core.scope;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+
+        prototypeBean1.addCount();
+        System.out.println("prototypeBean1.getCount() = " + prototypeBean1.getCount());
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        prototypeBean2.addCount();
+        System.out.println("prototypeBean2.getCount() = " + prototypeBean2.getCount());
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+
+    }
+
+
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count = clientBean1.logic();
+        assertThat(count).isEqualTo(1);
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        assertThat(count2).isEqualTo(2);
+        // 만약 logic안에서 prototypeBean을 받아서 호출하면 항상 1이다...
+//        int count3 = clientBean2.logic();
+//        assertThat(count3).isEqualTo(1);
+
+    }
+
+    @Scope("singleton")
+    static class ClientBean {
+        public PrototypeBean prototypeBean;
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("ClientBean.init");
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("ClientBean.close");
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+        public void addCount() {
+            count++;
+        }
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init = " + this);
+
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
