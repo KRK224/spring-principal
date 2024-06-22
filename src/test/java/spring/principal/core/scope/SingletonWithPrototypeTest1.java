055e4be54fb0496354b9637e985f969f6dbbc4ba
krk224 <hihas93@naver.com>
2024-06-22 12:18:19 +0900

    [feat] 프로토타입 스코프 - 싱글톤과 함께 사용할 경우의 문제점 ObjectProvider는 스프링에 의존적인 객체로 자동으로 빈을 등록해준다. 제공하는 기능은 DL(Dependency Lookup)로 스프링 컨테이너를 쉽게 조회할 수 있다. (프로토타입 빈만 조회하는 것이 아닌 모든 빈 조회 가능)

--- src/test/java/spring/principal/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/spring/principal/core/scope/SingletonWithPrototypeTest1.java
@@ -7,6 +7,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -39,7 +40,7 @@ public class SingletonWithPrototypeTest1 {
         assertThat(count).isEqualTo(1);
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
         // 만약 logic안에서 prototypeBean을 받아서 호출하면 항상 1이다...
 //        int count3 = clientBean2.logic();
 //        assertThat(count3).isEqualTo(1);
@@ -48,14 +49,16 @@ public class SingletonWithPrototypeTest1 {
 
     @Scope("singleton")
     static class ClientBean {
-        public PrototypeBean prototypeBean;
+//        public PrototypeBean prototypeBean;
+        public final ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
